a60f21252aa9f9bbb023a3d0217e9180e56dae13
rising-jun <jun36817@naver.com>
2022-08-02 16:46:49 +0900

    Initial Commit

---
+++ DoodleMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoodleMVVM
+//
+//  Created by 김동준 on 2022/08/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

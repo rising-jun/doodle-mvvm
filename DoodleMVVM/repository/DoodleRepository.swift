38859cc0b1d370646fdc15663de81a09488a4810
rising-jun <jun36817@naver.com>
2022-08-04 17:24:37 +0900

    [Chore] : 파일정리.

--- DoodleMVVM/repository/DoodleRepository.swift
+++ DoodleMVVM/repository/DoodleRepository.swift
@@ -11,27 +11,3 @@ import RxSwift
 protocol DoodleRepository {
     func fetchDoodles() -> Observable<[DoodleDTO]>
 }
-
-final class DoodleRepositoryImpl: DoodleRepository {
-    func fetchDoodles() -> Observable<[DoodleDTO]> {
-        guard let url = URL(string: "https://public.codesquad.kr/jk/doodle.json") else { return Observable.just([]) }
-        let urlRequest = URLRequest(url: url)
-        return URLSession.shared.rx
-            .data(request: urlRequest)
-            .decode(type: [DoodleDTO].self, decoder: JSONDecoder())
-    }
-}
-
-final class ImageManager {
-    private init() { }
-    static let shared = ImageManager()
-    func requestImage(url: String) -> Observable<Data> {
-        guard let url = URL(string: url) else { return Observable.just(Data()) }
-        let urlRequest = URLRequest(url: url)
-        return URLSession.shared.rx
-            .data(request: urlRequest)
-            .catch { error in
-                return Observable.just(Data())
-            }
-    }
-}
---
+++ DoodleMVVM/repository/ImageManager.swift
@@ -0,0 +1,23 @@
+//
+//  ImageManager.swift
+//  DoodleMVVM
+//
+//  Created by 김동준 on 2022/08/04.
+//
+
+import Foundation
+import RxSwift
+
+final class ImageManager {
+    private init() { }
+    static let shared = ImageManager()
+    func requestImage(url: String) -> Observable<Data> {
+        guard let url = URL(string: url) else { return Observable.just(Data()) }
+        let urlRequest = URLRequest(url: url)
+        return URLSession.shared.rx
+            .data(request: urlRequest)
+            .catch { error in
+                return Observable.just(Data())
+            }
+    }
+}
